1c6016fc9e1c2a613f79eb2eaa365ff37bbbfaa1
Andre Brdoch <andrebrdoch@yahoo.de>
2020-11-14 00:19:14 +0100

    basic rendering & routing

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     'comma-dangle': [2, 'always-multiline'],
+    'no-underscore-dangle': 0,
     'import/no-unresolved': 0,
+    'react/destructuring-assignment': 0,
     'react/jsx-filename-extension': 0,
   },
 };
--- plugins/schema-inspector/index.js
+++ plugins/schema-inspector/index.js
@@ -2,10 +2,13 @@ import { route } from 'part:@sanity/base/router';
 import { BiBook } from 'react-icons/bi';
 import Tool from './components/Tool';
 
+const router = route('/schemas', [route('/:schemaType/:schemaName')]);
+
 export default {
   title: 'Schema Inspector',
   name: 'schemas',
-  router: route('/:selectedDocumentId'),
+  router,
   icon: BiBook,
+  // Todo: pass options
   component: Tool,
 };
