be2911026ca5f108ebb242b0e0f80378b95a14b0
Andre Brdoch <andrebrdoch@yahoo.de>
2020-11-15 17:18:21 +0100

    basic inspector link setup

--- plugins/schema-inspector/components/Inspector.tsx
+++ plugins/schema-inspector/components/Inspector.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
 import ReactInspector from 'react-json-inspector';
-import styles from '@sanity/desk-tool/lib/panes/documentPane/inspectDialog/inspectDialog.css';
+import styles from './Inspector.css';
 import { TypeType } from '../types';
+
+const Link = (props) => <span className={styles.inspectorLink}>{props.children}</span>;
+
+const interactiveLabel = (props) => {
+  const { keypath, value, isKey } = props;
+  const pathParts = keypath.split('.');
+  const lastPart = pathParts[pathParts.length - 1];
+
+  console.log(props);
+  console.log(keypath);
+  console.log(lastPart);
+  console.log('----');
+
+  if (lastPart === 'type' && !isKey) {
+    return <Link>{value}</Link>;
+  }
+
+  return '';
+};
 
 interface Props {
   type: TypeType;
@@ -14,7 +33,7 @@ const Inspector = (props: Props) => {
     type && (
       <div className={styles.jsonInspectorContainer}>
         {/* too: make isExpanded configurable */}
-        <ReactInspector data={type} isExpanded={() => true} />
+        <ReactInspector data={type} isExpanded={() => true} interactiveLabel={interactiveLabel} />
       </div>
     )
   );
